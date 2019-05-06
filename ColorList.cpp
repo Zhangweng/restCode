a05f515bc1d9577535aa54239f96aff0f22d9ace
fzk <1958193501@qq.com>
2019-05-06 17:23:25 +0800

    Delete ColorList.cpp

--- ColorList.cpp
+++
@@ -1,7 +0,0 @@
-#include"ColorList.h"
-
-cv::Scalar Color::ColorBlue(255, 0, 0, 0);
-cv::Scalar Color::ColorRed(0, 255, 0, 0);
-cv::Scalar Color::ColorGreen(0, 0, 255, 0);
-cv::Scalar Color::ColorWhite(255, 255, 255, 0);
-cv::Scalar Color::ColorBlack(0, 0, 0, 0);
